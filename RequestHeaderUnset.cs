2dd45c4c63429901219da7d1bfcf8c1fbaaa75ed
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2024-01-13 03:28:48 -0500

    Initial commit

---
+++ RequestHeaderUnset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+
+namespace RequestHeaderUnset
+{
+    public class RequestHeaderUnset : IHttpModule
+    {
+        #region IHttpModule implementation
+
+        public void Filter(Object source, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)source;
+            HttpRequest request = app.Context.Request;
+            string conf = ConfigurationManager.AppSettings["RequestHeaderUnsetList"];
+            if (conf == null)
+                return;
+            string[] RequestHeaderUnsetList = conf.Split(',');
+            foreach (string h in RequestHeaderUnsetList)
+            {
+                request.Headers.Remove(h);
+            }
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(Filter);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        #endregion
+    }
+}
